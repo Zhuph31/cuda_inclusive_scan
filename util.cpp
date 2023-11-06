b0a71e2600f16fe4caff0417db44c7ed9189c406
Zhu  Penghui <zhupeng8@ug61.eecg.toronto.edu>
2023-11-06 10:15:25 -0500

    fake random

--- util.cpp
+++ util.cpp
@@ -16,11 +16,11 @@ void generateInput(int32_t *h_input, size_t size)
     int32_t sum = 0;
     for (size_t i = 0; i < size; ++i)
     {
-        int next_val;
-        do {
-            next_val = dist(gen);
-        } while (__builtin_add_overflow_p(sum, next_val, sum));
-        sum += next_val;
-        h_input[i] = next_val;
+      int next_val = 1;
+      // do {
+      //     next_val = dist(gen);
+      // } while (__builtin_add_overflow_p(sum, next_val, sum));
+      sum += next_val;
+      h_input[i] = next_val;
     }
 }
