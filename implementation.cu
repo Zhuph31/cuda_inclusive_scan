4deb6560b5ee4975e2b5d634e637e4f9f53f93e3
penghui.zhu <zhupeng8@ug61.eecg.toronto.edu>
2023-11-26 10:50:53 -0500

    rm useless sync

--- implementation.cu
+++ implementation.cu
@@ -7,7 +7,7 @@
 void printSubmissionInfo() {
   // This will be published in the leaderboard on piazza
   // Please modify this field with something interesting
-  char nick_name[] = "Penghui Zhu Steven";
+  char nick_name[] = "Evil olive";
 
   // Please fill in your information (for marking purposes only)
   char student_first_name[] = "Penghui";
@@ -206,7 +206,6 @@ void scan_equal(int *output, const int *input, int length, bool is_inclusive) {
 
   prescan_large<<<blocks, block_threads, 2 * sharedMemSize>>>(
       output, input, block_elems, sums, is_inclusive);
-  cudaDeviceSynchronize();
 
   // always scan incr in an exclusive mode
   if ((blocks + 1) / 2 > block_threads) {
@@ -214,7 +213,6 @@ void scan_equal(int *output, const int *input, int length, bool is_inclusive) {
   } else {
     scan_small(incr, sums, blocks, false);
   }
-  cudaDeviceSynchronize();
   add<<<blocks, block_elems>>>(output, block_elems, incr);
 
   cudaFree(sums);
@@ -228,7 +226,6 @@ void scan_large(int *output, const int *input, int length, bool is_inclusive) {
   if (remainder > 0) {
     scan_small(&(output[even_length]), &(input[even_length]), remainder,
                is_inclusive);
-    cudaDeviceSynchronize();
 
     if (is_inclusive) {
       add<<<1, remainder>>>(&(output[even_length]), &(output[even_length - 1]));
